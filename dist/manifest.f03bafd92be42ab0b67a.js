2c6549d3a04a87430952601b56b21e0670f02489
huskyhoochu <dfg1499@gmail.com>
2018-01-25 15:41:26 +0900

    webpack-dev-server 적용

--- dist/manifest.f03bafd92be42ab0b67a.js
+++ dist/manifest.f03bafd92be42ab0b67a.js
@@ -80,7 +80,7 @@
 /******/ 		if (__webpack_require__.nc) {
 /******/ 			script.setAttribute("nonce", __webpack_require__.nc);
 /******/ 		}
-/******/ 		script.src = __webpack_require__.p + "" + chunkId + "." + {"0":"d4872d901ebc8632d109","1":"a8308c981de491d1502c"}[chunkId] + ".js";
+/******/ 		script.src = __webpack_require__.p + "" + chunkId + "." + {"0":"d4872d901ebc8632d109","1":"b48343702fbb06e9c3f9"}[chunkId] + ".js";
 /******/ 		var timeout = setTimeout(onScriptComplete, 120000);
 /******/ 		script.onerror = script.onload = onScriptComplete;
 /******/ 		function onScriptComplete() {
