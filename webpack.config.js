bf1752a5007cdb87b278535bc860046e3f1ff2e8
huskyhoochu <dfg1499@gmail.com>
2018-01-25 14:20:41 +0900

    bundle & vendor splitting

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,20 @@
-var webpack = require('webpack');
-var path = require('path');
+const webpack = require('webpack');
+const path = require('path');
+
+const VENDOR_LIBS = [
+  'faker', 'lodash',
+  'react', 'react-dom', 'react-input-range', 'react-redux', 'react-router',
+  'redux', 'redux-form', 'redux-thunk',
+];
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    bundle: './src/index.js',
+    vendor: VENDOR_LIBS,
+  },
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: '[name].js',
   },
   module: {
     rules: [
@@ -17,7 +26,7 @@ module.exports = {
       {
         use: ['style-loader', 'css-loader'],
         test: /\.css$/,
-      }
+      },
     ],
   },
 };
